30a2250d26e5893ff1998b795c61feca10f4888b
kimyeonghwi <pwrkim12@gmail.com>
2021-08-16 20:54:23 +0900

    클론코딩 중간

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Movie App 2021 
 
 React JS Fundamentals course 2021
+
+
+# 클론코딩하면서 알아두면 좋을거 같은 것들 
+
+```js 
+function renderFood(dish) {
+  return <Food name={dish.name} picture={dish.img} babo={dish.aaa} />;
+}
+
+function App() {
+  console.log(food.map(renderFood))
+  return (
+    <div>
+      {food.map(renderFood)}
+    </div>
+    );  
+}
+//맵 함수를 이용해서 반복작업을 줄여서 각각의 컨포넌트 만들어 주기 
+//클론코딩책 p.96
+```
--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from 'prop-types'
 
-function App() {
+function Food({name , picture ,rating}) {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h2>I love {name}</h2>
+    <h4>{rating}/5 </h4>
+    <img src={picture} alt={name}/>
+  </div>
+  
   );
 }
+const food = [
+{ id: 1,
+  name : 'kimci',
+  img : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.E8lZ_WyIAXWARAgDLCtLRgHaFj%26pid%3DApi&f=1',
+  rating: 4.9,
+},
+{ id: 2,
+  name : '삼겹살 ',
+  img : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.E8lZ_WyIAXWARAgDLCtLRgHaFj%26pid%3DApi&f=1',
+  rating: 5,
+},
+{ id:3,
+  name : '우동',
+  img : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.E8lZ_WyIAXWARAgDLCtLRgHaFj%26pid%3DApi&f=1',
+  rating: 3,
+},
+
+];
+
+function App() {
+  
+  return (
+    <div>
+      
+      {food.map((dish) => (<Food key={dish.id} name={dish.name} picture={dish.img} rating={dish.rating}/>))}
+
+    </div>
+    );  
+}
+
+
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating : PropTypes.number.isRequired,
+};
 
 export default App;
